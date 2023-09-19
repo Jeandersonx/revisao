12e133ab95d4dc527de1f95c7edc502fece239ac
Jeanderson Moreira de Araújo <131190612+Jeandersonx@users.noreply.github.com>
2023-09-19 14:37:23 -0300

    Add files via upload

---
+++ ggmoreira/aula19-09/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> I love Brasil <h1>
+    <h2> O brasil é um país cheio de diversidade <h2>
+    <h3> Mas tem muito ladrão roubando tudo <h3>
+    <img src="flora.jpg" alt="" width= "30%">
+    <img src="osmutantes.jpg" alt="" width= "40%">
+    
+</body>
+</html>
